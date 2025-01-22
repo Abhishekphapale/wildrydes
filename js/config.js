0aeb9ab15dd8c6097e9c77c34fdda22c15b66eb0
Abhishekphapale <112766522+Abhishekphapale@users.noreply.github.com>
2025-01-22 13:01:18 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_tkrM5K9IA', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6rtik10e4va7mkuvof2ipaa96k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1efj0g34ithdc66eok0k4toici', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
